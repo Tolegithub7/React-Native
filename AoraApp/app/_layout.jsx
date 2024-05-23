eb50c74d382741b33f065d6a4162a2134787f937
Tolegithub7 <Tolekadima7@gmail.com>
2024-05-24 00:59:35 +0300

    changing the home page form layout to index.jst

--- AoraApp/app/_layout.jsx
+++ AoraApp/app/_layout.jsx
@@ -1,24 +1,8 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Slot } from "expo-router";
 
 const RootLayout = () => {
-  return (
-    <View>
-      <Text style={styles.container}>RootLayout</Text>
-    </View>
-  );
+  return <Slot />;
 };
 
 export default RootLayout;
-
-const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    color: "blue",
-    fontWeight: "bold",
-    fontSize: "30px",
-  },
-});
