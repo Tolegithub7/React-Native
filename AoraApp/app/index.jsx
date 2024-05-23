6be905d8ff068912d95256ccaa116c9924f911ff
Tolegithub7 <Tolekadima7@gmail.com>
2024-05-24 01:21:56 +0300

    link to profile from index home page

--- AoraApp/app/index.jsx
+++ AoraApp/app/index.jsx
@@ -1,11 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Aora my first app ever</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to Profile
+      </Link>
     </View>
   );
 }
