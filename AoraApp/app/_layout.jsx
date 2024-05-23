bedf1c8215205f2fccf7046a8a14a16b256b9b87
Tolegithub7 <Tolekadima7@gmail.com>
2024-05-24 01:16:54 +0300

    Stack alterantive option to route

--- AoraApp/app/_layout.jsx
+++ AoraApp/app/_layout.jsx
@@ -1,13 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 
 const RootLayout = () => {
   return (
-    <>
-      <Text>Header</Text>
-      <Slot />
-      <Text>Footer</Text>
-    </>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
   );
 };
 
