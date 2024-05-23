9ad61762cf711e8128eb37f54879f5b24e3b3e38
Tolegithub7 <Tolekadima7@gmail.com>
2024-05-23 21:55:04 +0300

    adding text and changing styles

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text1}>I worked for know</Text>
+      <Text style={styles.text2}>Funny right</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text1: {
+    fontSize: 12,
+    fontWeight: "bold",
+    color: "red",
+  },
+  text2: {
+    color: "green",
+    fontSize: 20,
+    fontFamily: "Helvetica",
   },
 });
