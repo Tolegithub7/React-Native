a983d664f99cea638a438b3578f0b22bd2c409ff
Tolegithub7 <Tolekadima7@gmail.com>
2024-05-24 02:29:12 +0300

    fuck nativewind is not working for web browser

--- AoraApp/app/index.jsx
+++ AoraApp/app/index.jsx
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aora my first app ever</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Aora!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}>
         Go to Profile
@@ -13,12 +13,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
