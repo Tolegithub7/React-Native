b7ccdef017747b1576365ca087729bb59518d83d
Tolegithub7 <Tolekadima7@gmail.com>
2024-05-24 02:52:29 +0300

    oww nativewind is not working and i'm back StyleSheet

--- AoraApp/app/index.jsx
+++ AoraApp/app/index.jsx
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Aora!</Text>
+    <View style={styles.container}>
+      <Text style={styles.text1}>Aora!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}>
         Go to Profile
@@ -13,3 +13,12 @@ export default function App() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    color: "blue",
+  },
+  text1: {
+    color: "red",
+  },
+});
