3c01d4849c327a2dc37adb9ba9eee280f5dc4df8
Tolegithub7 <Tolekadima7@gmail.com>
2024-05-24 16:28:00 +0300

    fucked with. ttf file

--- AoraApp/app/_layout.jsx
+++ AoraApp/app/_layout.jsx
@@ -1,7 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Slot, Stack } from "expo-router";
+import { useFonts } from "expo-font";
 
 const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+  });
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
