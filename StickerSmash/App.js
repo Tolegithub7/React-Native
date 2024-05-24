17a21e2a1a9ccd3b8aa26485d3c7790b56566d9a
Tolegithub7 <Tolekadima7@gmail.com>
2024-05-25 00:22:19 +0300

    app.js update

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text1}>I worked for know</Text>
+      <Text style={styles.text1}>Tole</Text>
       <Text style={styles.text2}>Funny right</Text>
       <StatusBar style="auto" />
     </View>
@@ -14,6 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
@@ -21,10 +22,10 @@ const styles = StyleSheet.create({
   text1: {
     fontSize: 40,
     fontWeight: "bold",
-    color: "blue",
+    color: "pink",
   },
   text2: {
-    color: "green",
+    color: "red",
     fontSize: 20,
     fontFamily: "Helvetica",
   },
